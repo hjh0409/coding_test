7c484a63cf79007d091f28819dbc4f20560f4501
hjh0409 <hjk7u.com@gmail.com>
2026-02-05 16:38:14 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+	long long n, m;
+	scanf("%lld %lld", &n, &m);
+	printf("%lld", llabs(n - m));
+	
+	return 0;
+}
