92776df92dc89be68a112a8f68ce4d7f14677725
hjh0409 <hjk7u.com@gmail.com>
2026-02-05 16:14:35 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.c
+++ 백준/Bronze/9498. 시험 성적/시험 성적.c
@@ -3,16 +3,16 @@
 int main() {
 	int score;
 	scanf("%d", &score);
-	if (score>=90 && score<=100) {
+	if (score>=90) {
 		printf("A");
 	}
-	else if (score>=80 && score<90) {
+	else if (score>=80) {
 		printf("B");
 	}
-	else if (score >= 70 && score < 80) {
+	else if (score >=70) {
 		printf("C");
 	}
-	else if (score >= 60 && score < 70) {
+	else if (score >=60) {
 		printf("D");
 }
 	else
