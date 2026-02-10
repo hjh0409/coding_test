bfb8c60afea3061990860067b248d2f880719db6
hjh0409 <hjk7u.com@gmail.com>
2026-02-10 17:44:08 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.c
+++ 백준/Bronze/9086. 문자열/문자열.c
@@ -6,19 +6,16 @@
 int main() {
     char str[1001];
     int n;
+    scanf("%d", &n);
     
-    scanf("%d",&n);
-    getchar();
-   
-    for (int i = 0; i < n; i++) {
-        int m = 0;
-        fgets(str, sizeof(str), stdin);
-        printf("%c",str[0]);
-        for (int j = 0; str[j] != '\0'; j++) {
-            m++;
-        }
-        printf("%c\n", str[m-2]);
+        
+        for (int i = 0; i < n; i++) {
+            scanf("%s", str);
+            int m;
+            m = strlen(str);
+            printf("%c%c\n", str[0], str[m - 1]);
     }
+    
 
     return 0;
 }
