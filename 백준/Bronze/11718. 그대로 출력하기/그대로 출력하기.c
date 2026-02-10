ca8fef5e2ab39ed2226305f8b018fb490ab6a05c
hjh0409 <hjk7u.com@gmail.com>
2026-02-10 12:29:41 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+ 
+
+int main() {
+    char str[101];
+
+    while (fgets(str, sizeof(str), stdin) != NULL){
+        printf("%s", str);
+}
+    return 0;
+}
